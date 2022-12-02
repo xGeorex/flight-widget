564a76c66ef5511b98a2fdded01bb27a56cb9af8
xGeorex <119671478+xGeorex@users.noreply.github.com>
2022-12-02 11:58:25 +0000

    Create Table.jsx

---
+++ src/components/Table.jsx
@@ -0,0 +1,14 @@
+import TableHead from './TableHead'
+import TableBody from './TableBody'
+
+const Table = () => {
+    return (
+        <table>
+            <TableHead />
+            <TableBody />
+
+        </table>
+    )
+}
+
+export default Table
